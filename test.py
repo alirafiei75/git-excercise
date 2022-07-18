fdfd4bb68fefd94cb97d9bfb5a4797c33c76849d
ali <a.rafiei1375@gmail.com>
2022-07-18 21:07:23 +0430

    test and index modified

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-# Creating an array using user input
-a = list()
 n = int(input("how many numbers you want your array to have?:\n"))
 for i in range (0, n):
     m = input("give me your numbers:\n")
     a.append(m)
+print(a)
+
